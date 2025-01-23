dc05de466e88cecbd91a3cec0464ee8fb79b1012
cat-ninja007 <150651906+cat-ninja007@users.noreply.github.com>
2025-01-23 13:50:01 +0800

    feat: Creating product card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,18 @@ import NavigationBar from "@/components/NavigationBar";
 import Header from "@/components/Header";
 import NewArrival from "@/components/NewArrival";
 import Footer from "@/components/Footer";
+import ProductCard from "@/components/ProductCard";
 
 const Home:FC = () => {
   return (
     <>
-      <div className="h-screen px-4 py-8 md:px-14">
+      <div className="h-screen px-4 py-8 md:px-14 relative">
         {/* <NavigationBar /> */}
         <Header />
+        <div className="flex gap-4">
+          <ProductCard />
+          <ProductCard />
+        </div>
         {/* <NewArrival /> */}
         {/* <Footer /> */}
       </div>
--- src/components/NavigationBar/index.tsx
+++ src/components/NavigationBar/index.tsx
@@ -20,49 +20,9 @@ const NavigationBar: FC = () => {
   );
 };
 
+
+
+
+
+
 export default NavigationBar;
-// import { FC } from "react";
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import { faMagnifyingGlass, faUser } from "@fortawesome/free-solid-svg-icons";
-// import Link from "next/link";
-// import {
-//   Navbar,
-//   NavbarBrand,
-//   NavbarCollapse,
-//   NavbarLink,
-//   NavbarToggle,
-// } from "flowbite-react"
-
-// const NavigationBar: FC = () => {
-//   return (
-//     <Navbar
-//       fluid
-//       className="absolute left-4 right-4 md:left-14 md:right-14 rounded-xl items-center z-10"
-//     >
-//       <NavbarToggle />
-//       <NavbarCollapse className="md:w-56 md:border-2">
-//         <NavbarLink href="#" active className="cursor">
-//           New Drops
-//         </NavbarLink>
-//         <NavbarLink as={Link} href="#" className="cursor">
-//           Men
-//         </NavbarLink>
-//         <NavbarLink as={Link} href="#" className="cursor">
-//           Women
-//         </NavbarLink>
-//       </NavbarCollapse>
-//       <NavbarBrand as={Link} href="/" className="text-center">
-//         <h2 className="font-black text-3xl cursor">ShelfMaster</h2>
-//       </NavbarBrand>
-
-//       <div className="navbar-container-right md:flex md:gap-5 md:w-56 md:border-2 item-center justify-end">
-//         <div className="search-icon-container hidden md:flex ">
-//           <FontAwesomeIcon icon={faMagnifyingGlass} className="cursor" />
-//         </div>
-//         <FontAwesomeIcon icon={faUser} className="cursor" />
-//       </div>
-//     </Navbar>
-//   );
-// };
-
-// export default NavigationBar;
