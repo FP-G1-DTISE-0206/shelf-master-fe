bea337a7a1be4a13aa37998b310e87b6ef822e59
goraasep <gora.asep@gmail.com>
2025-01-30 08:43:49 +0700

    fix: responsive layout

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -10,7 +10,7 @@ import { FC } from "react";
 
 const LoginPage: FC = () => {
   return (
-    <div className="flex items-center justify-center bg-white px-8">
+    <div className="flex items-center justify-center bg-white px-8 max-sm:py-8">
       <div className="w-full max-w-md">
         <h2 className="text-3xl font-bold text-center mb-6">Login</h2>
         <form className="space-y-4">
--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -8,7 +8,7 @@ import { FC } from "react";
 
 const RegisterPage: FC = () => {
   return (
-    <div className="flex items-center justify-center bg-white px-8">
+    <div className="flex items-center justify-center bg-white px-8 max-sm:py-8">
       <div className="w-full max-w-md">
         <h2 className="text-3xl font-bold text-center mb-6">
           Create an Account
