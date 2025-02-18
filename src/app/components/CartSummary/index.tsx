eeb741be04be26ae1bf30a77981f554b60f03b25
cat-ninja007 <150651906+cat-ninja007@users.noreply.github.com>
2025-02-18 11:34:27 +0800

    feat: tweaking zustand state, page responsivenss, and midtrans snap api

--- src/app/components/CartSummary/index.tsx
+++ src/app/components/CartSummary/index.tsx
@@ -1,81 +1,78 @@
-'use client'
+"use client";
 import { FC } from "react";
 import Link from "next/link";
 import ChoosenProduct from "./ChoosenProduct";
-import useCartStore from "@/hooks/useCartStore";
-import cartStore from "@/store/cartStore";
+import useCartStore from "@/store/cartStore";
 
-const CartSummary:FC = () => {
-  const cartItems = useCartStore((state) => state.cartItems);
-  
-  return(
-    <>
-      <div className="lg:grid lg:grid-cols-3 lg:gap-x-12">
-        
-        {/* Cart Item Section */}
-        <div className="w-full h-auto rounded-2xl bg-shelf-white lg:w-[90%] mb-6 p-4 lg:p-6 lg:col-span-2">
-          
-          <div className="lg:pb-10">
-            <h3 className="text-xl lg:text-3xl lg:pb-2 font-semibold">Your Bag</h3>
-            <p className="text-[14px] lg:text-base mb-2">
-              Items in your bag not reserved - check out now to make them yours
-            </p>
-          </div>
+const CartSummary: FC = () => {
+  const { cartItems, getTotalPrice } = useCartStore();
 
-          {/* <ChoosenProduct />
-          <ChoosenProduct />
-          <ChoosenProduct /> */}
-          {/* Render all chosen products dynamically */}
-          {cartStore.getState().cartItems.length > 0 ? (
-            cartStore.getState().cartItems.map((item) => (
-              <ChoosenProduct key={item.id} product={item} />
-            ))
-          ) : (
-            <p className="text-center text-gray-500">Your bag is empty.</p>
-          )}
+  return (
+    <div className="lg:grid lg:grid-cols-3 lg:gap-x-12">
+      {/* Cart Item Section */}
+      <div className="w-full h-auto rounded-2xl bg-shelf-white lg:w-[100%] mb-6 p-4 lg:p-6 lg:col-span-2">
+        <div className="lg:pb-10">
+          <h3 className="text-xl lg:text-3xl lg:pb-2 font-semibold">
+            Your Bag
+          </h3>
+          <p className="text-[14px] lg:text-base mb-2">
+            Items in your bag are not reserved—check out now to make them yours.
+          </p>
         </div>
 
-          {/* Order Summary Section */}
-        <div className="w-full h-[326px] rounded-2xl bg-shelf-white lg:bg-transparent p-4 lg:grow-3">
-          <h3 className="text-xl font-semibold lg:text-[32px]">Order Summary</h3>
-          <div className="w-full mx-auto mt-2">
-            <table className="w-full">
-              <tbody>
-                <tr className="lg:text-xl">
-                  <td className="py-2">{cartStore.getState().cartItems.length} ITEM(S)</td>
-                  <td className="text-right py-2">Rp {cartStore.getState().cartItems.reduce((total, item) => total + item.price * item.quantity, 0).toLocaleString("id-ID")}</td>
-                </tr>
-                <tr className="lg:text-xl">
-                  <td className="py-2">Delivery</td>
-                  <td className="text-right py-2">Rp 6,999</td>
-                </tr>
-                <tr className="lg:text-xl">
-                  <td className="py-2">Sales Tax</td>
-                  <td className="text-right py-2">-</td>
-                </tr>
-                <tr className="font-bold lg:text-2xl">
-                  <td className="py-2">Total</td>
-                  <td className="text-right py-2">
-                    Rp {cartStore.getState().cartItems.reduce((total, item) => total + item.price * item.quantity, 6999).toLocaleString("id-ID")}
-                  </td>
-                </tr>
-              </tbody>
-            </table>
+        {/* Choosen Products */}
+        {cartItems.length > 0 ? (
+          cartItems.map((item) => <ChoosenProduct key={item.id} product={item} />)
+        ) : (
+          <p className="text-center text-gray-500">Your bag is empty.</p>
+        )}
+      </div>
 
-            <button 
-              className="bg-shelf-black mt-4 xl:py-[15.5px] py-[13px] lg:px-10 px-[16px] w-full rounded-lg text-shelf-white xl:font-semibold font-medium xl:text-[14px] text-[12px]"
-            >
-            <Link href={"/payment"}>CHECKOUT</Link>
-          </button>
+      {/* Order Summary Section */}
+      <div className="w-full h-auto rounded-2xl bg-shelf-white lg:bg-transparent p-4 lg:grow-3">
+        <h3 className="text-xl font-semibold lg:text-[32px]">Order Summary</h3>
+        <div className="w-full mx-auto mt-2">
+          <table className="w-full">
+            <tbody>
+              <tr className="lg:text-xl">
+                <td className="py-2">{cartItems.length} ITEM(S)</td>
+                <td className="text-right py-2">
+                  Rp {getTotalPrice().toLocaleString("id-ID")}
+                </td>
+              </tr>
+              <tr className="lg:text-xl">
+                <td className="py-2">Delivery</td>
+                <td className="text-right py-2">Rp 6,999</td>
+              </tr>
+              <tr className="lg:text-xl">
+                <td className="py-2">Sales Tax</td>
+                <td className="text-right py-2">-</td>
+              </tr>
+              <tr className="font-bold lg:text-2xl">
+                <td className="py-2">Total</td>
+                <td className="text-right py-2">
+                  Rp {(getTotalPrice() + 6999).toLocaleString("id-ID")}
+                </td>
+              </tr>
+            </tbody>
+          </table>
+
+          {/* Checkout Button */}
+          <Link href="/payment">
+            <button className="bg-shelf-black mt-4 xl:py-[15.5px] py-[13px] lg:px-10 px-[16px] w-full rounded-lg text-shelf-white xl:font-semibold font-medium xl:text-[14px] text-[12px]">
+              CHECKOUT
+            </button>
+          </Link>
+
           <p className="pt-2 lg:text-xl">
-            <Link href="/promo" className="underline text-base">User a promo code</Link>
+            <Link href="/promo" className="underline text-base">
+              Use a promo code
+            </Link>
           </p>
-          </div>
-          
         </div>
       </div>
-    </>
-  )
-}
+    </div>
+  );
+};
 
 export default CartSummary;
--- src/app/components/ProductCard/index.tsx
+++ src/app/components/ProductCard/index.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import { FC } from "react";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
@@ -19,40 +19,30 @@ const ProductCard: FC<ProductCardProps> = ({
   discount,
 }) => {
   const router = useRouter();
-  // const formatPrice = (price: number): string => {
-  //   return price.toLocaleString("id-ID");
-  // };
 
   const formatPrice = (price?: number): string => {
-    if(typeof price !== "number" || isNaN(price)){
+    if (typeof price !== "number" || isNaN(price)) {
       return "Rp 0";
     }
-    return `Rp ${price.toLocaleString("id-ID")}`
-  }
+    return `Rp ${price.toLocaleString("id-ID")}`;
+  };
 
-  // const handleViewProduct = () => {
-  //   // console.log(`Product: ${name}\nPrice: Rp.${formatPrice(price)}`);
-  //   console.log({
-  //     name,
-  //     price: `Rp.${formatPrice(price)}`,
-  //     images,
-  //   });
-  // };
   const handleViewProduct = () => {
-    router.push(`/product/${id}`)
+    router.push(`/product/${id}`);
   };
   return (
     <>
       <div className="product-card-container w-full">
         <div className="product-image-container border-8 border-shelf-white rounded-3xl ">
-          <div className="hero-card-container relative rounded-2xl w-full lg:h-[334px] h-[164px] overflow-hidden">
-            <Image 
-              src={images?.[0] || "/images/kohceng-senam.jpg"} 
-              alt={name || "Product Image"} 
-              fill
-              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" 
-              priority
-              className="object-cover" />
+          <div className="hero-card-container relative rounded-2xl w-full h-full overflow-hidden">
+              <Image
+                src={images?.[0] || "/images/kohceng-senam.jpg"}
+                alt={name || "Product Image"}
+                width={500}
+                height={500}
+                className="object-cover rounded-2xl"
+              />
+            {/* </div> */}
             {discount && (
               <div className="product-status bg-shelf-blue w-auto h-auto flex items-center justify-center rounded-tl-2xl rounded-br-2xl absolute top-0">
                 <p className="text-shelf-white text-[12px] px-4 py-2 font-semibold">
@@ -73,7 +63,8 @@ const ProductCard: FC<ProductCardProps> = ({
               onClick={handleViewProduct}
               className="bg-shelf-black xl:py-[15.5px] py-[13px] lg:px-10 px-[16px] w-full rounded-lg text-shelf-white xl:font-semibold font-medium xl:text-[14px] text-[12px]"
             >
-              VIEW PRODUCT <br/><span className="text-shelf-orange">{formatPrice(price)}</span>
+              VIEW PRODUCT <br />
+              <span className="text-shelf-orange">{formatPrice(price)}</span>
             </button>
           </div>
         </div>
